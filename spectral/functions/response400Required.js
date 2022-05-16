dccfbcf8b8a16c4fa7642f27fbefe722efde2027
Bert Ramakers <bertramakers@me.com>
2022-05-16 17:08:06 +0200

    Add rule that 400 responses to requests with bodies must mention common body error types

--- spectral/functions/response400Required.js
+++ spectral/functions/response400Required.js
@@ -1,5 +1,5 @@
 export default (input) => {
-  if ('requestBody' in input && !('responses' in input && '400' in input.responses)) {
+  if (!('responses' in input && '400' in input.responses)) {
     return [
       {
         message: 'Operations with requestBody must have a response with status code 400'
