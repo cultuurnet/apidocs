09279b564450ae03159808f6eebedc1acab8c76c
Bert Ramakers <bertramakers@me.com>
2023-01-24 16:53:30 +0100

    Add more echo's for debugging

--- .github/actions/stoplight-clean-branches/action.sh
+++ .github/actions/stoplight-clean-branches/action.sh
@@ -5,6 +5,8 @@ USERNAME=$2
 PASSWORD=$3
 PROJECT_ID=$4
 
+echo "Fetching JWT..."
+
 TOKEN=$(curl -s --location --request POST 'https://publiq.stoplight.io/api/v1/auth/login' \
 --header 'Content-Type: application/json' \
 --data-raw "{
@@ -14,13 +16,10 @@ TOKEN=$(curl -s --location --request POST 'https://publiq.stoplight.io/api/v1/au
     \"source\": \"web\"
 }" | jq -r '.token')
 
-#echo $TOKEN
-#echo $PROJECT_ID
+echo "Successfully fetched JWT."
 
 HIDDEN_BRANCHES=$(curl -s --location --request GET "https://publiq.stoplight.io/api/v1/projects/${PROJECT_ID}/branches" \
 --header "Authorization: Bearer ${TOKEN}" | jq -r -c '.items[] | select(.is_published == false) | @base64')
-
-#echo $HIDDEN_BRANCHES
 
 for BRANCH in $HIDDEN_BRANCHES; do
     _jq() {
@@ -32,14 +31,14 @@ for BRANCH in $HIDDEN_BRANCHES; do
    BRANCH_ID_PREFIXED=$(echo ${BRANCH_ID_ENCODED} | base64 --decode)
    BRANCH_ID=${BRANCH_ID_PREFIXED#$BRANCH_ID_PREFIX}
 
-   echo $BRANCH_SLUG
-   echo $BRANCH_ID
+   echo "Checking branch with slug ${BRANCH_SLUG} and (base64 decoded) id ${BRANCH_ID}..."
 
    EXISTS=$(git ls-remote --heads origin ${BRANCH_SLUG})
 
    if [[ -z ${EXISTS} ]]; then
-    echo "branch ${BRANCH_SLUG} should be removed"
+    echo "Branch ${BRANCH_SLUG} does not exist in GitHub, removing it from Stoplight..."
+    echo "Branch ${BRANCH_SLUG} successfully removed from Stoplight."
   else
-    echo "branch ${BRANCH_SLUG} should be kept"
+    echo "branch ${BRANCH_SLUG} still exists in GitHub, keeping it on Stoplight."
   fi
 done
