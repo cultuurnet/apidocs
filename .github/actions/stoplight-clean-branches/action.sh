7b66f6b54dc2aaccc7f360421f68dd505b817645
Bert Ramakers <bertramakers@me.com>
2023-01-24 16:26:31 +0100

    Make bash script fetch a token and the branches of a project

--- .github/actions/stoplight-clean-branches/action.sh
+++ .github/actions/stoplight-clean-branches/action.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 
 WORKSPACE_ID=$1
+USERNAME=$2
+PASSWORD=$3
+PROJECT_ID=$4
 
-echo $WORKSPACE_ID
+TOKEN=$(curl -s --location --request POST 'https://publiq.stoplight.io/api/v1/auth/login' \
+--header 'Content-Type: application/json' \
+--data-raw "{
+    \"workspaceIntegrationId\": ${WORKSPACE_ID},
+    \"usernameOrEmail\": \"${USERNAME}\",
+    \"password\": \"${PASSWORD}\",
+    \"source\": \"web\"
+}" | jq -r '.token')
+
+#echo $TOKEN
+#echo $PROJECT_ID
+
+HIDDEN_BRANCHES=$(curl -s --location --request GET "https://publiq.stoplight.io/api/v1/projects/${PROJECT_ID}/branches" \
+--header "Authorization: Bearer ${TOKEN}" | jq -r -c '.items[] | select(.is_published == false) | @base64')
+
+#echo $HIDDEN_BRANCHES
+
+for BRANCH in $HIDDEN_BRANCHES; do
+    _jq() {
+     echo ${BRANCH} | base64 --decode | jq -r ${1}
+    }
+   BRANCH_SLUG=$(_jq '.slug')
+   BRANCH_ID_PREFIX='br:'
+   BRANCH_ID_PREFIXED=$(echo $(_jq '.id') | base64 --decode)
+   BRANCH_ID=${BRANCH_ID_PREFIXED#$BRANCH_ID_PREFIX}
+
+   #echo $BRANCH_SLUG
+   #echo $BRANCH_ID
+
+   EXISTS=git ls-remote --heads origin ${BRANCH_SLUG}
+
+   if [[ -z ${EXISTS} ]]; then
+    echo "branch ${SLUG} should be kept"
+  else
+    echo "branch ${SLUG} should be removed"
+  fi
+done
